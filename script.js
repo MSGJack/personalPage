77f149bca8b2f7188123536c72a689348f43ff96
Jack <fakegenki@gmail.com>
2022-08-04 13:25:53 -0700

    hi

---
+++ script.js
@@ -0,0 +1,13 @@
+const links = [
+    'https://www.youtube.com/watch?v=P-F7v8d7Vzc',
+    'https://www.youtube.com/watch?v=8i6-PgW-uX0',
+    'https://www.youtube.com/watch?v=JIc5vXcD4yY',
+    'https://www.youtube.com/watch?v=-TWnAO7vBns',
+    'https://www.youtube.com/watch?v=uOw58cc6GQc',
+    'https://www.youtube.com/watch?v=jGkBFIw5iMk',
+]
+
+const someLink = () => {
+    let randomPick = Math.floor(Math.random() * (links.length));
+    document.getElementById("displayLink").innerHTML = links[randomPick];
+}    
